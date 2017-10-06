4b859268deef5522ff9ddb85deb038530792ec00
Aleksey Linnik <dom_istorika@mail.ru>
2017-10-06 12:55:31 +0700

    Сложно всё!

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,7 +10,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!!");            
+            Console.WriteLine("Hello World!!!!");
+            Console.WriteLine("МММать, как всё сложно!");           
             Console.ReadKey();
         }
     }
